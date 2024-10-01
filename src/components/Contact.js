b9b4c96057310d00cba82906be421e2e83b868bf
krishna <krishnabaddam02>
2024-10-01 15:59:26 +0530

    changed name from krishna Reddy to Krishna Reddy Baddam

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ const Contact = ()=>{
         <div>
             <h1 className="text-lg font-bold p-5 m-5 underline">Contact Us</h1>
             <div className="m-4 p-4 ">
-                <h1 className="text-md font-semibold text-indigo-500 py-5">Name : Krishna Reddy </h1>
+                <h1 className="text-md font-semibold text-indigo-500 py-5">Name : Krishna Reddy Baddam</h1>
                 <h1 className="text-md font-semibold text-indigo-500 py-5"> Phone No: +917997914001</h1>
                 <h1 className="text-md font-semibold text-indigo-500 py-5"> Email : krishnabaddam676@gmail.com</h1>
             </div>
